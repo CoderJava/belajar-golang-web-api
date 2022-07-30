53817c09a99b5aec3bb4aeb19f36cb2301ba399a
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-07-30 21:32:35 +0700

    Belajar Menampilkan Error Validasi

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 func main() {
@@ -56,9 +58,11 @@ func postBooksHandler(c *gin.Context) {
 
 	err := c.ShouldBindJSON(&bookInput)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, err)
-		fmt.Println(err)
-		return
+		for _, e := range err.(validator.ValidationErrors) {
+			errorMessage := fmt.Sprintf("Error on field %s, condition %s", e.Field(), e.ActualTag())
+			c.JSON(http.StatusBadRequest, errorMessage)
+			return
+		}
 	}
 
 	c.JSON(http.StatusOK, gin.H{
@@ -69,7 +73,7 @@ func postBooksHandler(c *gin.Context) {
 }
 
 type BookInput struct {
-	Title    string `json:"title" binding:"required"`
-	Price    int    `json:"price" binding:"required,number"`
-	SubTitle string `json:"sub_title"`
+	Title    string      `json:"title" binding:"required"`
+	Price    json.Number `json:"price" binding:"required,number"`
+	SubTitle string      `json:"sub_title"`
 }
